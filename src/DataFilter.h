e348fc8a4871d13f16e6af149c52170b290fccfa
liangzhuohua <liangzhuohua@genew.com>
2023-03-10 15:39:23 +0800

    add data filter

--- src/DataFilter.h
+++ src/DataFilter.h
@@ -19,7 +19,7 @@ public:
         static string passMd5 = MD5(passStr).rawdigest();
         static auto *hp = reinterpret_cast<uint64_t *>(passMd5.data());
         static auto *lp = reinterpret_cast<uint64_t *>(passMd5.data() + sizeof(uint64_t));
-        static uint64_t pass = buf->size()%2?*hp:*lp;
+        uint64_t pass = buf->size()%2?*hp:*lp;
         auto * data = reinterpret_cast<uint8_t *>(buf->data());
         for (int offset = 0; offset + sizeof(uint64_t) < buf->size(); offset+= sizeof(uint64_t)){
             *((uint64_t*)(data+offset)) = *((uint64_t*)(data+offset))^pass;
