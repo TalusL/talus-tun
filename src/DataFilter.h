06cb84f7ee5b0a7221a105726fed71c93ddf55d4
liangzhuohua <liangzhuohua@genew.com>
2023-03-13 16:17:45 +0800

    cast

--- src/DataFilter.h
+++ src/DataFilter.h
@@ -17,8 +17,8 @@ public:
     static void Filter(const Buffer::Ptr &buf){
         static string passStr = mINI::Instance()[CONFIG_PASS];
         static string passMd5 = MD5(passStr).rawdigest();
-        static auto *hp = reinterpret_cast<uint64_t *>(passMd5.data());
-        static auto *lp = reinterpret_cast<uint64_t *>(passMd5.data() + sizeof(uint64_t));
+        static auto *hp = reinterpret_cast<const uint64_t *>(passMd5.data());
+        static auto *lp = reinterpret_cast<const uint64_t *>(passMd5.data() + sizeof(uint64_t));
         uint64_t pass = buf->size()%2?*hp:*lp;
         auto * data = reinterpret_cast<uint8_t *>(buf->data());
         for (int offset = 0; offset + sizeof(uint64_t) < buf->size(); offset+= sizeof(uint64_t)){
